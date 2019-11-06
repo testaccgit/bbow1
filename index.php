1970db07007a8fbca2535db0f6cfd276f24d001c
Boris Boskovic <neocort@gmail.com>
2019-11-06 14:19:44 +0100

    bbo1-webhook

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <title>BBOisBEST</title>
  </head>
  <body>
- <?php echo '<p>JenkinsGitWebhook</p>'; ?> 
+ <?php echo '<p>Jenkins-GitWebhook</p>'; ?> 
  <?php echo '<p>BBO is BEST</p>'; ?> 
  </body>
 </html>
